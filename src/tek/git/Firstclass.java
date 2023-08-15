ef397993056f673fc1cc7e4ffc732ed9342e8933
maryam <maryam.zakizadeh23@gmail.com>
2023-08-15 10:32:13 -0400

    initial project changes

---
+++ src/tek/git/Firstclass.java
@@ -0,0 +1,9 @@
+package tek.git;
+
+public class Firstclass {
+	public static void main(String[] args ) {
+		System.out.println("Hello Guardians");
+	
+	}
+
+}
---
+++ src/tek/git/SecondClassFromNewBranch.java
@@ -0,0 +1,8 @@
+package tek.git;
+
+public class SecondClassFromNewBranch {
+
+	public static void main(String[] args) {
+		System.out.println("This is changees from my new balance");
+	}
+}
